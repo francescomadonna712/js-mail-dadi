fa2537e3e71f4bb56a1b2b683046aee2abf0b28d
francesco madonna <fra.mad.93@hotmail.it>
2024-04-22 16:10:42 +0200

    modifiche dadi

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-
+//bottone
 const button = document.getElementById ('button');
 
 button.addEventListener('click',function() {
+    //math random player
 const player = Math.floor(Math.random() * 6);
+
 console.log( player);
 
+player1.innerText = (player);
+
  
-  
+  //math random cpu
 const cpu = Math.floor(Math.random() * 6);
+
 console.log( cpu);
 
+cpu1.innerText = (cpu);
+//dichiarazione risultato
 const result = document.getElementById("result");
-  
+  //casistiche
     if(player === cpu) {
     result.innerText = ('Pareggio!')
   }else if(player > cpu){
